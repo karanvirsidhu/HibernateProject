6b409dc1ace9b83354a2f7ebbf589e2360cf866c
Karanvir <iamkaranvir93@gmail.com>
2019-02-08 12:47:33 -0500

    Annotation and retrieving object

--- src/org/karanvir/dto/UserDetails.java
+++ src/org/karanvir/dto/UserDetails.java
@@ -1,13 +1,47 @@
 package org.karanvir.dto;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity (name = "User_Details")
 public class UserDetails {
     @Id
+    @Basic
     private int userId;
+    @Column(name = "User_Name")
     private String userName;
+    @Temporal(TemporalType.DATE)
+    private Date dateOfJoining;
+
+    private String address;
+    @Lob
+    private String description;
+
+    public Date getDateOfJoining() {
+        return dateOfJoining;
+    }
+
+    public void setDateOfJoining(Date dateOfJoining) {
+        this.dateOfJoining = dateOfJoining;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
 
     public int getUserId() {
         return userId;
--- src/org/karanvir/hibernate/HibernateTest.java
+++ src/org/karanvir/hibernate/HibernateTest.java
@@ -5,12 +5,17 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.karanvir.dto.UserDetails;
 
+import java.util.Date;
+
 public class HibernateTest {
 
     public static void main(String[] args){
         UserDetails user = new UserDetails();
-        user.setUserId(2);
-        user.setUserName("Kanav");
+        user.setUserId(1);
+        user.setUserName("Karanvir");
+        user.setDateOfJoining(new Date());
+        user.setAddress("Montreal");
+        user.setDescription("Master of software engineering");
 
         SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
         Session session = sessionFactory.openSession();
@@ -19,6 +24,13 @@ public class HibernateTest {
         session.getTransaction().commit();
         session.close();
 
+        user =null;
+
+        session = sessionFactory.openSession();
+        session.beginTransaction();
+        session.get(UserDetails.class, 1);
+        session.getTransaction().commit();
+        session.close();
 
     }
 }
